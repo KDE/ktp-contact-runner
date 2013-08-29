46197ec51402e4469a7a65f8c6f08424584c13af
l10n daemon script <scripty@kde.org>
2013-08-29 03:55:45 +0000

    SVN_SILENT made messages (.desktop file)

--- plasma-runner-ktp-contact.desktop.cmake
+++ plasma-runner-ktp-contact.desktop.cmake
@@ -1,6 +1,18 @@
 [Desktop Entry]
 Name=Instant Messaging
+Name[it]=Messaggistica istantanea
+Name[nl]=Instant Messaging
+Name[sl]=Hipno sporočanje
+Name[sv]=Direktmeddelandekontakter
+Name[uk]=Миттєвий обмін повідомленнями
+Name[x-test]=xxInstant Messagingxx
 Comment=Start a chat with any of your IM contacts or change your IM status
+Comment[it]=Avvia una chat con un tuo contatto di MI oppure cambia il tuo stato di MI
+Comment[nl]=Begin een chat met een van uw IM-contactpersonen of wijzig uw IM-status
+Comment[sl]=Začnite klepet s katerimkoli izmed vaših stikov hipnega sporočanja ali spremenite vaše stanje
+Comment[sv]=Börja chatta med någon direktmeddelandekontakt eller ändra din direktmeddelandestatus
+Comment[uk]=Почати спілкування з будь-яким з ваших контактів або змінити ваш запис стану у системах обміну повідомленнями
+Comment[x-test]=xxStart a chat with any of your IM contacts or change your IM statusxx
 Icon=user-online
 
 X-KDE-ServiceTypes=Plasma/Runner
